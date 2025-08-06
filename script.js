7b4eaa499d54b7d839fbdb1f171b874ce72b942c
JuanoBQ <juanjo.bravo3@gmail.com>
2025-08-06 15:58:39 -0500

    updatev04

--- script.js
+++ script.js
@@ -137,6 +137,12 @@ startAutoSlide2();
 carousel2.addEventListener('mouseenter', stopAutoSlide2);
 carousel2.addEventListener('mouseleave', startAutoSlide2);
 
+
+
+
+
+
+
 // Funcionalidad del carrito de compras
 document.addEventListener('DOMContentLoaded',function() {
   if(!localStorage.getItem('carrito')) {
@@ -200,6 +206,78 @@ function updateCartCounter() {
 
   // Actualizar contador al cargar la página
   updateCartCounter();
+
+
+
+  // ============= CAROUSEL =================
+
+
+  const slides = document.querySelectorAll(
+  "#carouselExampleIndicators .carousel-item"
+);
+const indicators = document.querySelectorAll(
+  "#carouselExampleIndicators .carousel-indicators button"
+);
+const prevBtn = document.querySelector(".carousel-control-prev");
+const nextBtn = document.querySelector(".carousel-control-next");
+let currentSlide = 0;
+let autoSlideInterval;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+  indicators.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+    dot.setAttribute("aria-current", i === index ? "true" : "false");
+  });
+  currentSlide = index;
+}
+
+function nextSlide() {
+  let next = currentSlide + 1;
+  if (next >= slides.length) next = 0;
+  showSlide(next);
+}
+
+function prevSlide() {
+  let prev = currentSlide - 1;
+  if (prev < 0) prev = slides.length - 1;
+  showSlide(prev);
+}
+
+function startAutoplay() {
+  autoSlideInterval = setInterval(nextSlide, 3000); // 3 segundos
+}
+
+function stopAutoplay() {
+  clearInterval(autoSlideInterval);
+}
+
+// Eventos
+nextBtn.addEventListener("click", () => {
+  nextSlide();
+  stopAutoplay();
+  startAutoplay();
+});
+
+prevBtn.addEventListener("click", () => {
+  prevSlide();
+  stopAutoplay();
+  startAutoplay();
+});
+
+indicators.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    showSlide(i);
+    stopAutoplay();
+    startAutoplay();
+  });
+});
+
+// Iniciar
+showSlide(0);
+startAutoplay();
 
 
 
